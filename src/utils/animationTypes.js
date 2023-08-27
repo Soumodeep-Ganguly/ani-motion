a2b56dc69baf0e727a6cef01d3af7b2850fd6251
Soumodeep-Ganguly <soumodeep.ganguly.007@gmail.com>
2023-08-27 12:53:37 +0530

    spin-flip-elastic-pulse-blink-shake-zigzag-spring

--- src/utils/animationTypes.js
+++ src/utils/animationTypes.js
@@ -2,7 +2,9 @@ import {
     animationListBorder, animationListBackground, animationListScale, animationListRotate, 
     animationListFade, animationListColorChange, animationListSlideUp, animationListSlideDown, 
     animationListSlideRight, animationListSlideLeft, animationListZoomIn, animationListZoomOut, 
-    animationListBounce
+    animationListBounce, animationListSpin, animationListFlip, animationListElastic, 
+    animationListPulse, animationListBlink, animationListShake, animationListSpinningLoader, 
+    animationListZigzag, animationListSpring, animationListFadeIn, animationListFadeOut
 } from './animationFunctions';
 
 const animationTypes = {
@@ -19,6 +21,17 @@ const animationTypes = {
     zoomIn: (settings) => animationListZoomIn(settings),
     zoomOut: (settings) => animationListZoomOut(settings),
     bounce: (settings) => animationListBounce(settings),
+    spin: (settings) => animationListSpin(settings),
+    flip: (settings) => animationListFlip(settings),
+    elastic: (settings) => animationListElastic(settings),
+    pulse: (settings) => animationListPulse(settings),
+    blink: (settings) => animationListBlink(settings),
+    shake: (settings) => animationListShake(settings),
+    spinner: (settings) => animationListSpinningLoader(settings),
+    zigzag: (settings) => animationListZigzag(settings),
+    spring: (settings) => animationListSpring(settings),
+    fadeIn: (settings) => animationListFadeIn(settings),
+    fadeOut: (settings) => animationListFadeOut(settings),
     // Add more animation types here
 };
 
--- src/utils/keyframes.js
+++ src/utils/keyframes.js
@@ -117,6 +117,177 @@ const zoomOutKeyframe = () => {
     </style>
 }
 
+const springKeyframe = () => {
+    return <style>
+        {`
+            @keyframes spring {
+                0%, 100% {
+                    transform: translateY(0);
+                }
+                20%, 80% {
+                    transform: translateY(-10px);
+                }
+                40%, 60% {
+                    transform: translateY(5px);
+                }
+            }
+        `}
+    </style>
+}
+
+const zigzagKeyframe = () => {
+    return <style>
+        {`
+            @keyframes zigzag {
+                0%, 100% {
+                    transform: translateX(0);
+                }
+                25%, 75% {
+                    transform: translateX(10px);
+                }
+                50% {
+                    transform: translateX(-10px);
+                }
+            }
+        `}
+    </style>
+}
+
+const spinKeyframe = () => {
+    return <style>
+        {`
+            @keyframes spin {
+                0% {
+                    transform: rotate(0);
+                }
+                100% {
+                    transform: rotate(360deg);
+                }
+            }
+        `}
+    </style>
+}
+
+const shakeKeyframe = () => {
+    return <style>
+        {`
+            @keyframes shake {
+                0%, 100% {
+                    transform: translateX(0);
+                }
+                10%, 30%, 50%, 70%, 90% {
+                    transform: translateX(-10px);
+                }
+                20%, 40%, 60%, 80% {
+                    transform: translateX(10px);
+                }
+            }
+        `}
+    </style>
+}
+
+const blinkKeyframe = () => {
+    return <style>
+        {`
+            @keyframes blink {
+                0%, 100% {
+                    opacity: 1;
+                }
+                50% {
+                    opacity: 0;
+                }
+            }
+        `}
+    </style>
+}
+
+const pulseKeyframe = () => {
+    return <style>
+        {`
+            @keyframes pulse {
+                0%, 100% {
+                    transform: scale(1);
+                }
+                50% {
+                    transform: scale(1.1);
+                }
+            }
+        `}
+    </style>
+}
+
+const flipKeyframe = () => {
+    return <style>
+        {`
+            @keyframes flip {
+                0% {
+                    transform: perspective(400px) rotateY(0);
+                }
+                40% {
+                    transform: perspective(400px) rotateY(170deg);
+                }
+                50% {
+                    transform: perspective(400px) rotateY(190deg);
+                }
+                80% {
+                    transform: perspective(400px) rotateY(340deg);
+                }
+                100% {
+                    transform: perspective(400px) rotateY(360deg);
+                }
+            }
+        `}
+    </style>
+}
+
+const elasticKeyframe = () => {
+    return <style>
+        {`
+            @keyframes elastic {
+                0%, 100% {
+                    transform: scaleX(1);
+                }
+                20%, 50%, 80% {
+                    transform: scaleX(1.25);
+                }
+                40%, 60% {
+                    transform: scaleX(0.85);
+                }
+            }
+        `}
+    </style>
+}
+
+const fadeInKeyframe = () => {
+    return <style>
+        {`
+            @keyframes fadeIn {
+                0% {
+                    opacity: 0;
+                }
+                100% {
+                    opacity: 1;
+                }
+            }
+        `}
+    </style>
+}
+
+const fadeOutKeyframe = () => {
+    return <style>
+        {`
+            @keyframes fadeOut {
+                0% {
+                    opacity: 1;
+                }
+                100% {
+                    opacity: 0;
+                }
+            }
+        `}
+    </style>
+}
+
 export default function keyframes(animationType) {
     if(animationType === "bounce") return bounceKeyframe()
     if(animationType === "slideUp") return slideUpKeyframe()
@@ -125,6 +296,16 @@ export default function keyframes(animationType) {
     if(animationType === "slideRight") return slideRightKeyframe()
     if(animationType === "zoomOut") return zoomOutKeyframe()
     if(animationType === "zoomIn") return zoomInKeyframe()
+    if(animationType === "spring") return springKeyframe()
+    if(animationType === "zigzag") return zigzagKeyframe()
+    if(animationType === "spinner") return spinKeyframe()
+    if(animationType === "shake") return shakeKeyframe()
+    if(animationType === "blink") return blinkKeyframe()
+    if(animationType === "pulse") return pulseKeyframe()
+    if(animationType === "flip") return flipKeyframe()
+    if(animationType === "elastic") return elasticKeyframe()
+    if(animationType === "fadeIn") return fadeInKeyframe()
+    if(animationType === "fadeOut") return fadeOutKeyframe()
     
     return null
 }
